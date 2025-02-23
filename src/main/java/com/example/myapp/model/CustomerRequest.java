43131dd16a6489aa4028937d6b1cb48fda05517c
SunilDevlops <sunilpatrocse@gmail.com>
2025-02-22 23:00:19 -0500

    adding curd operations logic

---
+++ src/main/java/com/example/myapp/model/CustomerRequest.java
@@ -0,0 +1,17 @@
+package com.example.myapp.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class CustomerRequest {
+
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into CUSTOMER_T (VERSION, FIRST_NAME, MIDDLE_NAME, LAST_NAME, EMAIL_ADDRESS, COUNTRY_CODE, AREA_CODE, PREFIX, LINE_NUMBER) values (0, 'Dollie', 'R.', 'Adams', 'dollie.adams@gmail.com', '+1',  301, 583, 2304);
+insert into CUSTOMER_T (VERSION, FIRST_NAME, MIDDLE_NAME, LAST_NAME, EMAIL_ADDRESS, COUNTRY_CODE, AREA_CODE, PREFIX, LINE_NUMBER) values (0, 'Cornelia', 'J.', 'Andresen', 'cornelia.andresen@gmail.com', '+1', 240, 384, 9732);
+insert into CUSTOMER_T (VERSION, FIRST_NAME, MIDDLE_NAME, LAST_NAME, EMAIL_ADDRESS, COUNTRY_CODE, AREA_CODE, PREFIX, LINE_NUMBER) values (0, 'Coral', 'Villareal', 'Betancourt', 'coral.betancourt@yahoo.com', '+1', 813, 234, 6442);
+insert into CUSTOMER_T (VERSION, FIRST_NAME, MIDDLE_NAME, LAST_NAME, EMAIL_ADDRESS, COUNTRY_CODE, AREA_CODE, PREFIX, LINE_NUMBER) values (0, 'Michael', 'J.', 'Grover', 'michael.grover@gmail.com', '+1', 240, 576, 2369);
+insert into CUSTOMER_T (VERSION, FIRST_NAME, MIDDLE_NAME, LAST_NAME, EMAIL_ADDRESS, COUNTRY_CODE, AREA_CODE, PREFIX, LINE_NUMBER) values (0, 'John', 'D.', 'Mudra', 'john.mudra@gmail.com', '+1', 301, 684, 9273);
+
