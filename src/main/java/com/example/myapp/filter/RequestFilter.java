016afa42bb65bffb44384b6f02650a1f24fd121e
SunilDevlops <sunilpatrocse@gmail.com>
2025-02-28 03:12:06 -0500

    added minikube kubernetes configuration

--- src/main/java/com/example/myapp/filter/RequestFilter.java
+++ src/main/java/com/example/myapp/filter/RequestFilter.java
@@ -6,13 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.filter.OrderedFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
 @Component
-public class RequestFilter extends OncePerRequestFilter {
+public class RequestFilter extends OncePerRequestFilter implements Ordered {
     @Value("${spring.profiles.active}")
     private String activeProfile;
 
@@ -43,4 +45,8 @@ public class RequestFilter extends OncePerRequestFilter {
         MDC.clear();
     }
 
+    @Override
+    public int getOrder() {
+        return 2;
+    }
 }
--- src/main/java/com/example/myapp/model/CustomerResponse.java
+++ src/main/java/com/example/myapp/model/CustomerResponse.java
@@ -2,10 +2,14 @@ package com.example.myapp.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerResponse {
     private Integer id;
     private String firstName;
